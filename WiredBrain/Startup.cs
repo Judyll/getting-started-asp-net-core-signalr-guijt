5d994c150bca18434ebe006988c6923944028f73
Judyll <judyll.agan@gmail.com>
2020-08-22 16:57:00 +0800

    Adding the wired brain project.

---
+++ WiredBrain/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using WiredBrain.Helpers;
+using WiredBrain.Hubs;
+
+namespace WiredBrain
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton(new Random());
+            services.AddSingleton<OrderChecker>();
+            services.AddHttpContextAccessor();
+            services.AddSignalR().AddMessagePackProtocol();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+
+            app.UseSignalR(routes => routes.MapHub<CoffeeHub>("/coffeehub"));
+            app.UseMvc();
+        }
+    }
+}
